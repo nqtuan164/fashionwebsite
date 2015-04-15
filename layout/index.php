6c03380ff60f5a7dc66a74e689d1c3a8c98e01a9
biixel <biixellexiid@gmail.com>
2015-04-15 17:08:48 +0700

    update html,css&img index.php

--- layout/index.php
+++ layout/index.php
@@ -2,15 +2,100 @@
 <html>
 <head>
 	<title>Thời trang MIXXO</title>
+	<!-- 	<link rel="stylesheet" href="css/style.css"> -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap.css.map">
+	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="css/style.css">
-	
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+
 </head>
 <body>
-<div class="container">
-	<div id="main-content" class="row">
-		
+	<!-- LOGO -->
+	<div id="header" class="row">
+		<div id="logo" class="container">
+			<a href="#">
+				<img id="img-logo" class="img-responsive" src="img/logo.png" height="94" width="268" alt="">
+			</a>
+		</div>
 	</div>
-</div>
+	<!-- END of LOGO -->
+	<!-- MENU -->
+	<div id="menu">
+		<div class="nav-menu">
+			<nav class="navbar container navbar-default">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Trang Chủ</a>
+				</div>
+				<div id="main-menu" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="#">Thời Trang Nam</a></li>
+						<li><a href="#">Thời Trang Nữ</a></li>
+						<li><a href="#">Liên Hệ</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<!-- END of MENU -->
+	<!-- MAIN CONTENT -->
+	<div id="main-content" class="row">
+	<div class="container">
+		<!-- SLIDE BANNER -->
+		<div class="slide-banner col-md-12 nopadding">
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				</ol>
+
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner" role="listbox">
+					<div class="item active">
+						<img src="img/3.jpg" alt="...">
+						<div class="carousel-caption">
+							Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts Read More
+						</div>
+					</div>
+					<div class="item">
+						<img src="img/2.jpg" alt="...">
+						<div class="carousel-caption">
+							Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts Read More
+						</div>
+					</div>
+				</div>
+
+				<!-- Controls -->
+				<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
+		</div>
+		<!-- END of SLIDE BANNER -->
+		<!-- PRODUCT -->
+		<div class="product col-md-12 nopadding">
+			<div class="col-md-3">
+				<img src="" alt="" class="img-responsive">
+			</div>
+		</div>
+		<!-- END of PRODUCT -->
+	</div>
+	</div>
+	<!-- END of MAIN CONTENT -->
 
 </body>
 </html>
