8ba3adc6dd8b3a3a390af9d740c8b40d610515df
biixel <biixellexiid@gmail.com>
2015-04-16 17:19:46 +0700

    update .product-list

--- layout/index.php
+++ layout/index.php
@@ -1,53 +1,53 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Thời trang MIXXO</title>
-	<!-- 	<link rel="stylesheet" href="css/style.css"> -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/bootstrap.css.map">
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/style.css">
-	<script src="js/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/script.js"></script>
+<title>Thời trang MIXXO</title>
+<!-- 	<link rel="stylesheet" href="css/style.css"> -->
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/bootstrap.css.map">
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
 
 </head>
 <body>
-	<!-- LOGO -->
-	<div id="header" class="row">
-		<div id="logo" class="container">
-			<a href="#">
-				<img id="img-logo" class="img-responsive" src="img/logo.png" height="94" width="268" alt="">
-			</a>
-		</div>
+<!-- LOGO -->
+<div id="header" class="row">
+	<div id="logo" class="container">
+		<a href="#">
+			<img id="img-logo" class="img-responsive" src="img/logo.png" height="94" width="268" alt="">
+		</a>
 	</div>
-	<!-- END of LOGO -->
-	<!-- MENU -->
-	<div id="menu">
-		<div class="nav-menu">
-			<nav class="navbar container navbar-default">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">Trang Chủ</a>
-				</div>
-				<div id="main-menu" class="navbar-collapse collapse">
-					<ul class="nav navbar-nav">
-						<li><a href="#">Thời Trang Nam</a></li>
-						<li><a href="#">Thời Trang Nữ</a></li>
-						<li><a href="#">Liên Hệ</a></li>
-					</ul>
-				</div>
-			</nav>
-		</div>
+</div>
+<!-- END of LOGO -->
+<!-- MENU -->
+<div id="menu">
+	<div class="nav-menu">
+		<nav class="navbar container navbar-default">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Trang Chủ</a>
+			</div>
+			<div id="main-menu" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="#">Thời Trang Nam</a></li>
+					<li><a href="#">Thời Trang Nữ</a></li>
+					<li><a href="#">Liên Hệ</a></li>
+				</ul>
+			</div>
+		</nav>
 	</div>
-	<!-- END of MENU -->
-	<!-- MAIN CONTENT -->
-	<div id="main-content" class="row">
+</div>
+<!-- END of MENU -->
+<!-- MAIN CONTENT -->
+<div id="main-content" class="row">
 	<div class="container">
 		<!-- SLIDE BANNER -->
 		<div class="slide-banner col-md-12 nopadding">
@@ -87,14 +87,79 @@
 		</div>
 		<!-- END of SLIDE BANNER -->
 		<!-- PRODUCT -->
-		<div class="product col-md-12 nopadding">
-			<div class="col-md-3">
-				<img src="img/2.jpg" alt="" class="img-responsive">
+		<div class="product-list col-md-12 nopadding">
+			<div class="col-md-3 col-sm-6 product">
+				<div class="col-md-12 product-content">
+					<a class="product-img col-md-12 nopadding" href="#">
+						<img src="img/product1.jpg" alt="" class="img-responsive">
+					</a>
+					<h3>Váy màu trắng bi</h3>
+					<h5>500.000VND</h5>
+					<p><a class="btn btnChiTiet btn-default" href="#" role="button">Chi tiết</a></p>
+				</div>
 			</div>
+			<div class="col-md-3 col-sm-6 product">
+				<div class="col-md-12 product-content">
+					<a class="product-img col-md-12 nopadding" href="#">
+						<img src="img/product2.jpg" alt="" class="img-responsive">
+					</a>
+					<h3>Váy màu trắng bi</h3>
+					<h5>500.000VND</h5>
+					<p><a class="btn btnChiTiet btn-default" href="#" role="button">Chi tiết</a></p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 product">
+				<div class="col-md-12 product-content">
+					<a class="product-img col-md-12 nopadding" href="#">
+						<img src="img/product1.jpg" alt="" class="img-responsive">
+					</a>
+					<h3>Váy màu trắng bi</h3>
+					<h5>500.000VND</h5>
+					<p><a class="btn btnChiTiet btn-default" href="#" role="button">Chi tiết</a></p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 product">
+				<div class="col-md-12 product-content">
+					<a class="product-img col-md-12 nopadding" href="#">
+						<img src="img/product1.jpg" alt="" class="img-responsive">
+					</a>
+					<h3>Váy màu trắng bi</h3>
+					<h5>500.000VND</h5>
+					<p><a class="btn btnChiTiet btn-default" href="#" role="button">Chi tiết</a></p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 product">
+				<div class="col-md-12 product-content">
+					<a class="product-img col-md-12 nopadding" href="#">
+						<img src="img/product1.jpg" alt="" class="img-responsive">
+					</a>
+					<h3>Váy màu trắng bi</h3>
+					<h5>500.000VND</h5>
+					<p><a class="btn btnChiTiet btn-default" href="#" role="button">Chi tiết</a></p>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 product">
+				<div class="col-md-12 product-content">
+					<a class="product-img col-md-12 nopadding" href="#">
+						<img src="img/product1.jpg" alt="" class="img-responsive">
+					</a>
+					<h3>Váy màu trắng bi</h3>
+					<h5>500.000VND</h5>
+					<p><a class="btn btnChiTiet btn-default" href="#" role="button">Chi tiết</a></p>
+				</div>
+			</div>
+
 		</div>
 		<!-- END of PRODUCT -->
 	</div>
+</div>
+<!-- END of MAIN CONTENT -->
+<!-- FOOTER -->
+<div id="footer">
+	<div class="container">
+		
 	</div>
-	<!-- END of MAIN CONTENT -->
+</div>
+<!-- END of FOOTER -->
 </body>
 </html>
